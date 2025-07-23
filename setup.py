9479d1949bb1e4c5e3454c150f4fc3ccb0d7848a
Rathodpradip1 <pradiprathod45@gmail.com>
2025-07-23 10:34:43 +0530

    project stucture ready, venv set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pradip Rathod",
+    author_email="pradiprathod45@gmail.com",
+    packages=find_packages()
+
+)    
